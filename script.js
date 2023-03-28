205d481b68ca6a90bd590dea21375d1cf0403678
vijaykumarcse007 <125144797+vijaykumarcse007@users.noreply.github.com>
2023-03-28 12:19:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,21 @@
 function palindrome(str){
 	var a=str;
 	let len=str.length;
+	var count1=0;
+	var count2=0;
 	for(var i=0;i<len;i++){
 		if(str.charAt[i]===str.charAt[len-1-i]){
-			return true;
+			count1+=1;
 		}
 		else{
-			return false;
+			count2+=1;
 		}
+	}
+	if(count2===1){
+		return false;
+	}
+	else{
+		return true;
 	}
 }
 module.exports = palindrome
